84e07c9fd4d1e2734e692e5cf2708846396f9b1b
initiald-tl52 <d.sch.d.52@gmail.com>
2019-10-14 22:27:55 +0900

    add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'wether/index'
+  root 'wether#index'
   get 'wether/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
