3f034779f8822543c98cc0e8d3951f73b6942d64
initiald-tl52 <d.sch.d.52@gmail.com>
2019-10-17 21:28:35 +0900

    change region.rb has many wether

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,5 @@
 class Region < ApplicationRecord
   has_many :users
   has_many :messages
-  belongs_to :wether
+  has_many :wether
 end
--- db/migrate/20191014130418_create_regions.rb
+++ db/migrate/20191014130418_create_regions.rb
@@ -3,7 +3,7 @@ class CreateRegions < ActiveRecord::Migration[5.2]
     create_table :regions do |t|
       t.string :name
       t.string :record_point
-
+      
       t.timestamps
     end
   end
