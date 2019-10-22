e6844806a9c03e0d2618169b600987a77f6b1a91
initiald-tl52 <d.sch.d.52@gmail.com>
2019-10-22 09:18:13 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_063705) do
+ActiveRecord::Schema.define(version: 2019_10_21_130046) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_10_20_063705) do
     t.string "record_point"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "alfabet_name"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
