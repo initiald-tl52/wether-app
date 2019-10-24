72e9adb897dd33fb24eb3574b38e0e31e509ed45
initiald-tl52 <d.sch.d.52@gmail.com>
2019-10-24 12:54:44 +0900

    revert

--- app/assets/javascripts/wether.js
+++ app/assets/javascripts/wether.js
@@ -1,7 +1,6 @@
 $(function() {
-  const API_KEY = "5bcd49f66a50a0d68ef2ed5d7526700e";
-  const FORCAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast?q=";
-  const CURRENT_BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+  var API_KEY = "5bcd49f66a50a0d68ef2ed5d7526700e";
+  var BASE_URL = "http://api.openweathermap.org/data/2.5/forecast";
 
   const context = $("#chart");
   const chart = new Chart(context,{
@@ -28,8 +27,19 @@ $(function() {
                                       }]
                                     }
                                   }
-                                });
-  
+                                  });
+
+  function appendProduct(data) {
+    var html_all =``;
+    var name_html = `<h2>${data.city.name}</h2>`;
+    html_all += name_html;
+    for (  var i = 0;  i < 5;  i++  ) {
+      var html = `<p>${data.list[i].main.temp}</p>`
+      html_all += html;
+    }
+    return html_all;
+  }
+
   function formatData(data){
     return formatedData = {
       labels : data.dates,
@@ -43,86 +53,22 @@ $(function() {
     }
   }
 
-  function appendTable(data) {
-    var dates = []
-    var wether_icons_id = []
-    var tempratures = []
-    var rains_per_3h = []
-    for (  var i = 0;  i < 5;  i++  ) {
-      dates.push(data.list[i].dt_txt);
-      console.log("data.list[i].dt_txt   "+data.list[i].dt_txt);
-      wether_icons_id.push(data.list[i].wether[0].icon);
-      console.log("data.list[i].wether[0].icon   "+data.list[i].wether[0].icon);
-      tempratures.push("data.list[i].main.temp   "+data.list[i].main.temp);
-      console.log("data.list[i].main.temp   "+data.list[i].main.temp);
-      rains_per_3h.push(data.list[i].rain.3h);
-      console.log("data.list[i].rain.3h   "+data.list[i].rain.3h);
-    }
-    var table = `<table border="1">
-                  <tbody>
-                      <tr class="tr_date">
-                      <td>${dates[0]}</td>
-                      <td>${dates[1]}</td>
-                      <td>${dates[2]}</td>
-                      <td>${dates[3]}</td>
-                      <td>${dates[4]}</td>
-                    </tr>
-                    <tr class="tr_wether">
-                      <td>${wether_icons_id[0]}</td>
-                      <td>${wether_icons_id[1]}</td>
-                      <td>${wether_icons_id[2]}</td>
-                      <td>${wether_icons_id[3]}</td>
-                      <td>${wether_icons_id[4]}</td>
-                    </tr>
-                    <tr class="tr_temprature">
-                      <td>${tempratures[0]} ℃</td>
-                      <td>${tempratures[1]} ℃</td>
-                      <td>${tempratures[2]} ℃</td>
-                      <td>${tempratures[3]} ℃</td>
-                      <td>${tempratures[4]} ℃</td>
-                    </tr>
-                    <tr class="tr_precipitation">
-                      <td>${rains_per_3h[0]} %</td>
-                      <td>${rains_per_3h[1]} %</td>
-                      <td>${rains_per_3h[2]} %</td>
-                      <td>${rains_per_3h[3]} %</td>
-                      <td>${rains_per_3h[4]} %</td>
-                    </tr>
-                  </tbody>
-                </table>`
-    return table;
-  }
-  function getWetherIcon(id){
-    return `http://openweathermap.org/img/w/${id}.png`
-  }
   $(".form_region").on("submit", function(e) {
     e.preventDefault();
     var city = $('#record_point').val();
     var show_url = $(this).attr('action');
-    let current_wether_url = CURRENT_BASE_URL + city +",jp&units=metric&APPID=" + API_KEY;
-    
-    // let forcast_wether_url = FORCAST_BASE_URL +city+",jp&units=metric&APPID=" + API_KEY;
-    // $.ajax({
-    //   type: 'GET',
-    //   url: forcast_wether_url,
-    //   dataType: 'json',
-    // })
-    // .done(function(data){
-    //   $('.under-side').empty();
-    //   $('.under-side').append(appendTable(data));
+
+    // // var open_wether_url = BASE_URL + "?q="+city+",jp&units=metric&APPID=" + API_KEY;
+    // // $.ajax({
+    // //   type: 'GET',
+    // //   url: open_wether_url,
+    // //   dataType: 'json',
+    // // })
+    // // .done(function(data){
+    // //   $('.info').empty();
+    // //   $('.info').append(appendProduct(data));
     // })
 
-    $.ajax({
-      type: 'GET',
-      url: current_wether_url,
-      dataType: 'json',
-    })
-    .done(function(data){
-      $('.wether').text(data.weather[0].main);
-      $('#temprature').text(data.main.temp);
-      $('#precipitation').text(data.main.humidity);
-      $('.imagebox').children('#wether-icon').attr('src', getWetherIcon(data.weather[0].icon));
-    })
     $.ajax({
       type: 'GET',
       url: show_url,
