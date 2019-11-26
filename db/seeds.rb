797fb5ad4301ef7afa14edade2ae58de826f31a4
initiald-tl52 <d.sch.d.52@gmail.com>
2019-11-26 21:53:01 +0900

    change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require "csv"
 
 CSV.foreach( 
-            '/db/20190918-1019北海道気象データ.csv', headers: true
+            '/app/db/20190918-1019北海道気象データ.csv', headers: true
             ) do |row|
   unless Region.exists?(record_point: row['地点名'])
     r = Region.new(
