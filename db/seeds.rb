4ae5feebd1e7b18b6338e76c70bcfeb2510460fd
initiald-tl52 <d.sch.d.52@gmail.com>
2019-11-26 21:23:25 +0900

    change path

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require "csv"
 
-CSV.foreach(Rails.root + 
-            'db/20190918-1019北海道気象データ.csv ', headers: true
+CSV.foreach( 
+            '/db/20190918-1019北海道気象データ.csv ', headers: true
             ) do |row|
   unless Region.exists?(record_point: row['地点名'])
     r = Region.new(
