b9d6c87d0049cc3473c7b517f5e188a9dd91c255
initiald-tl52 <d.sch.d.52@gmail.com>
2019-12-02 16:03:50 +0900

    change selecter design

--- app/assets/javascripts/wether.js
+++ app/assets/javascripts/wether.js
@@ -123,9 +123,11 @@ $(function() {
     return `http://openweathermap.org/img/w/${id}.png`
   }
 
-  $("#record_point").change(function(e) {
-    e.preventDefault();
-    let city = $('#record_point').val();
+  $(".custom-select-trigger").on('DOMSubtreeModified propertychange', function() {
+    if($('.custom-select-trigger').text()===""){
+      return false;
+    };
+    let city = $('.custom-select').val();
     let show_url = "/wether/show"
     let current_wether_url = CURRENT_BASE_URL + city +",jp&units=metric&APPID=" + API_KEY;
     let open_wether_url = FORCAST_BASE_URL +city+",jp&units=metric&APPID=" + API_KEY;
